10afd38652203bc540a0ff75c5e3b0f755c00b42
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-05-03 17:56:06 +0300

    bug-fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,18 +11,35 @@ stopButton.disabled = true;
 startButton.addEventListener('click', onStartButtonClick);
 stopButton.addEventListener('click', onStopButtonClick);
 
+// function onStartButtonClick() {
+//     startButton.disabled = true;
+//     stopButton.disabled = false;
+//     intervalID = setInterval(() => {
+//         document.body.style.backgroundColor = getRandomHexColor()
+//     }, 1000);
+// };
+
+// function onStopButtonClick() {
+//     clearInterval(intervalID);
+//     startButton.disabled = false;
+//     stopButton.disabled = true;
+// };
+
 function onStartButtonClick(event) {
-    startButton.disabled = true;
-    stopButton.disabled = false;
+
+    event.target.setAttribute('disabled', "");
+    stopButton.removeAttribute('disabled');
+
     intervalID = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor()
     }, 1000);
+
 };
 
-function onStopButtonClick() {
+function onStopButtonClick(event) {
+    
     clearInterval(intervalID);
-    startButton.disabled = false;
-    stopButton.disabled = true;
+    event.target.setAttribute('disabled', "");
+    startButton.removeAttribute('disabled');
+
 };
-
-
